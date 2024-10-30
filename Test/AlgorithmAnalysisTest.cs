e77ff12a7be8d2463550489af9510cdb8ce70e89
chitangchin <chitangb.chin@gmail.com>
2024-10-30 13:07:43 -0400

    Merged Main and Local

--- Test/AlgorithmAnalysisTest.cs
+++ Test/AlgorithmAnalysisTest.cs
@@ -15,5 +15,11 @@ namespace AlgorithmAnalysisTests
         {
             Assert.That(AlgorithmAnalysis.TimeComplexityTwo(), Is.EqualTo(29));
         }
+        public void TestingTimeComplexityThree()
+        {
+            Assert.That(AlgorithmAnalysis.TimeComplexityThree([1,2,3,4,5,6,7,8], 7), Is.EqualTo(1));
+            Assert.That(AlgorithmAnalysis.TimeComplexityThree([1, 2, 3, 4, 5, 6, 7, 8], 10), Is.EqualTo(0));
+            Assert.That(AlgorithmAnalysis.TimeComplexityThree([1, 2, 3, 4, 5, 6, 7, 8, 100, 200], 100), Is.EqualTo(1));
+        }
     }
 }
